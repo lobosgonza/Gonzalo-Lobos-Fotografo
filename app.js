64ba5f88e258736d3a111dbbabd31050a01618b2
unknown <lobosgonza60@gmail.com>
2020-07-13 20:37:43 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+
+var app = express()
+
+app.use(express.static("public"))
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/trabajos", function(req,res){
+  res.sendFile(__dirname + "/trabajos.html")
+})
+
+app.listen(3000,function (req,res) {
+console.log("Server is running on port 3000")
+  })
