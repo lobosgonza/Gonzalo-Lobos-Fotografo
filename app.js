40eef1460cfbb1a2086c6328704f119ab7303c75
unknown <lobosgonza60@gmail.com>
2020-07-14 14:43:58 -0400

    Add Procfile cambio server update img

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/trabajos", function(req,res){
   res.sendFile(__dirname + "/trabajos.html")
 })
 
-app.listen(3000,function (req,res) {
+app.listen(process.env.PORT || 3000, function (req,res) {
 console.log("Server is running on port 3000")
   })
